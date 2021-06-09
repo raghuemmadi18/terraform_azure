89bcef840a2403097fd8cc43b93714b0bfbb671f
venureddy909 <venu.unix.com>
2021-06-08 22:32:57 -0500

    terraform15 with alias in differfent tenents/accounts and subscription for Lab7.9

--- lab_work/terraform-on-azcloud-master/Sec07/Lab7.9/main.tf
+++ lab_work/terraform-on-azcloud-master/Sec07/Lab7.9/main.tf
@@ -1,30 +1,32 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "2.41.0"
-      configuration_aliases = [ azurerm.alt ]
+      source                = "hashicorp/azurerm"
+      version               = "2.41.0"
+      configuration_aliases = [azurerm.alt]
     }
   }
   backend "azurerm" {
-    resource_group_name  = "Backend_rg"
-    storage_account_name = "backendsa01"
+    resource_group_name  = "Terra-rg"
+    storage_account_name = "venuremotesa01fs"
     container_name       = "tfstate"
-    key                  = "statefilename.tfstate"
+    key                  = "multi_tenet.tfstate"
   }
 }
 provider "azurerm" {
   features {}
-  subscription_id = "default-sub_id"
+  subscription_id = "6b9cdc1c-0ed8-4812-8154-bb975b91d702"
+  tenant_id       = "ea5bc0a6-fc86-41ae-b3ad-0f4e5a37658b"
 }
 
 provider "azurerm" {
   alias = "alt"
   features {}
-  subscription_id = "second-sub-id"
+  subscription_id = "53126cf4-dd12-46e1-899e-92f0f1edc5c4"
+  tenant_id       = "c848aaf0-e3e2-40f0-bd40-e20b8921000d"
 }
 
-resource "azurerm_resource_group" "example1" { 
+resource "azurerm_resource_group" "example1" {
   name     = "provider1"
   location = "East US"
 }
@@ -36,7 +38,7 @@ resource "azurerm_resource_group" "example2" {
 }
 
 resource "azurerm_storage_account" "example2" {
-  provider = azurerm.alt
+  provider                 = azurerm.alt
   name                     = "testsao1234"
   resource_group_name      = azurerm_resource_group.example2.name
   location                 = azurerm_resource_group.example2.location

b533583a34d7bae79d8688608d6e8d233657d23b
venureddy909 <venu.unix.com>
2021-07-24 22:16:58 -0500

    workspace-Test

--- lab_work/terraform-on-azcloud-master/Sec10/Lab10.1/backend.tf
+++ lab_work/terraform-on-azcloud-master/Sec10/Lab10.1/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "Terra-rg"
-    storage_account_name = "remotesa01"
+    storage_account_name = "remotestrgvenu"
     container_name       = "tfstate"
   }
 }
--- lab_work/terraform-on-azcloud-master/Sec10/Lab10.1/backend.tfvars
+++ lab_work/terraform-on-azcloud-master/Sec10/Lab10.1/backend.tfvars
@@ -1,3 +1,5 @@
+# For Prod env="Prod"
+# For Test env="Test"
 key                  = "#{key}#"
 #for test key ws-test-Lab10.1.tfstate
 #for prod key ws-prod-Lab10.1.tfstate
